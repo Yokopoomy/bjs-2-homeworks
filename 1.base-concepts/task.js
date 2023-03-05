4d12d4bfe0049dcd976a424bc31b6c5e316faade
Evgeny Golovin <yokopoomy20@gmail.com>
2023-03-05 18:15:19 +0300

    Finished solution

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,29 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = Math.pow(b, 2)-4*a*c;
+  if (d < 0) {
+  } else if (d === 0) {
+    arr[0] = -b/(2*a);
+  } else {
+    arr[0] = (-b + Math.sqrt(d) )/(2*a)
+    arr[1] = (-b - Math.sqrt(d) )/(2*a)
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  if (isNaN(percent)) {
+    return false;
+  } else if (isNaN(contribution)) {
+    return false;
+  } else if (isNaN(amount)) {
+    return false;
+  }
+  percent = percent/100/12;
+  let body = amount - contribution;
+  let monthlyPayment = body * (percent + (percent / (((1 + percent) ** countMonths) - 1)));
+  let allPayment = monthlyPayment * countMonths;
+  return parseFloat(allPayment.toFixed(2));
 }
