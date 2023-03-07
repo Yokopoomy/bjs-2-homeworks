0b8ce02183a7e81cf56e46942c4c1163e02511e6
Evgeny Golovin <yokopoomy20@gmail.com>
2023-03-07 14:37:57 +0300

    Delete else getAverage

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -18,11 +18,10 @@ Student.prototype.addMarks = function (...marks) {
 Student.prototype.getAverage = function () {
   if (!(this.hasOwnProperty('marks')) || this.marks.length === 0) {
     return 0;
-  } else {
-    let average = 0;
-    this.marks.forEach((mark) => average += mark/this.marks.length);
-    return average;
   }
+  let average = 0;
+  this.marks.forEach((mark) => average += mark/this.marks.length);
+  return average;
 }
 
 Student.prototype.exclude = function (reason) {
