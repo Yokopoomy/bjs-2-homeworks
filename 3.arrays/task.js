3ce33814e4a59c2b40c0d011b5ff82313bbaa2b2
Evgeny Golovin <yokopoomy20@gmail.com>
2023-05-03 16:33:13 +0300

    Решения: 3 массивы и 8 декораторы

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
 function compareArrays(arr1, arr2) {
-  
+  return arr1.length == arr2.length && arr1.every((el,i)=> el === arr2[i])
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let result = users.filter(item => item.gender === gender).reduce((acc, item, index, arr) => {
+      acc += item.age;
+      if(index === arr.length -1){
+          return acc / arr.length;
+      }
+      return acc;
+  }, 0)
+  return result;
 }
--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,68 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+  let cache = [];
+  function wrapper(...args) {
+    const hash = md5(args);
+    let inCache = cache.find((item) => item.hash === hash);
+      if(inCache){
+        console.log("Из кэша: " + inCache.value);
+        return "Из кэша: " + inCache.value;
+      }
+    const result = func(...args);
+    cache.push({hash:hash, value:result});
+    if(cache.length > 5){
+      cache.splice(0,1);
+      console.log('Удалили из кэша');
+    }
+    console.log("Вычисляем: " + func(...args));
+    return "Вычисляем: " + func(...args);
+  }
+  return wrapper
+}
+
+//Решение эксперта
+function cachingDecoratorNew(func) {
+  let cache = [];
+  const maxCacheValuesCount = 5;
+  return (...args) => {
+    const hash = md5(args);
+    const objectFromCache = cache.find(object => object.hash === hash);
+    if (objectFromCache){
+      console.log("Из кеша: ", objectFromCache.value);
+      return "Из кэша: " + objectFromCache.value;
+    }
+
+    const value = func(...args);
+    cache.push({hash, value})
+    if(cache.length > maxCacheValuesCount) {
+      cache.shift();
+    }
+
+    console.log("Вычисляем: ", value);
+    return "Вычисляем: " + value;
+  };
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+  let timeoutId = null;
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+
+  function wrapper(...args) {
+    wrapper.allCount++;
+
+    if(timeoutId === null) {
+      func(...args);
+      wrapper.count++;
+    }
+
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      wrapper.count++;
+      func(...args);
+    }, delay);
+  }
+
+  return wrapper;
 }
